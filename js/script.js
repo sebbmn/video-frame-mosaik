cd33df84bd2856aab1e660e75acf5bd381de7a55
sebbmn <sebbmn@gmail.com>
2018-04-11 00:12:54 +0200

    feat: replace timeupdate with setInterval

--- js/script.js
+++ js/script.js
@@ -1,25 +1,44 @@
 let video = document.getElementById("video");
 let thumbnailsMosaik = document.getElementById("thumbnails");
+
+let isPlaying = false;
 let maxTime = 0;
 
+video.addEventListener('play', function() {
+  isPlaying = true;
+});
+video.addEventListener('suspend', function() {
+  isPlaying = false;
+});
 video.addEventListener('timeupdate', function() {
-  if(video.currentTime > maxTime) {
-    maxTime = video.currentTime;
-    addThumbnail(video.currentTime);
-  }
+
 }, false);
 
-function addThumbnail(currentTime) {
+window.setInterval(function() {
+  if(isPlaying) {
+    if(video.currentTime > maxTime) {
+      maxTime = video.currentTime;
+      addThumbnail(video.currentTime, 0);
+    }
+  }
+}, 50);
+
+function addThumbnail(currentTime, xStart) {
   let thumbnailCanvas = document.createElement("canvas");
   thumbnailCanvas.width = 48;
   thumbnailCanvas.height = 48;
-  thumbnailCanvas.id = currentTime;
-  thumbnailCanvas.className = "thumbnail";
-  thumbnailCanvas.onclick = () => { 
+
+  let context = thumbnailCanvas.getContext("2d");
+
+  context.drawImage(video,xStart,0,400,400,0,0,48,48);
+  dataUrl = thumbnailCanvas.toDataURL();
+  thumbnailImage = document.createElement('img');
+  thumbnailImage.src = dataUrl;
+  thumbnailImage.className = "thumbnail";
+
+  thumbnailImage.onclick = () => { 
     video.currentTime = currentTime; 
   };
 
-  let context = thumbnailCanvas.getContext("2d");
-  context.drawImage(video,0,0,48,48);
-  thumbnailsMosaik.appendChild(thumbnailCanvas);
+  thumbnailsMosaik.appendChild(thumbnailImage);
 }
