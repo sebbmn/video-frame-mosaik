599ddd42eddfd31a61946eb09ecdb29b46d30ea1
sebbmn <sebbmn@gmail.com>
2018-04-11 15:12:26 +0200

    feat: add incremental hue variation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let video = document.getElementById("video");
 let thumbnailsMosaik = document.getElementById("thumbnails");
+let hueVariation = 0;
 
 let isPlaying = false;
 let maxTime = 0;
@@ -29,8 +30,10 @@ function addThumbnail(currentTime, xStart) {
   thumbnailCanvas.height = 48;
 
   let context = thumbnailCanvas.getContext("2d");
-
+  context.filter = `hue-rotate(${hueVariation}deg)`;
+  hueVariation +=1;
   context.drawImage(video,xStart,0,400,400,0,0,48,48);
+  
   dataUrl = thumbnailCanvas.toDataURL();
   thumbnailImage = document.createElement('img');
   thumbnailImage.src = dataUrl;
